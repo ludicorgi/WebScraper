5a94e8ac14966ada3409c6fb47b115c3cb148be6
ludicorgi <69206279+ludicorgi@users.noreply.github.com>
2022-12-03 01:15:48 +0800

    getlinks and getemail functions

--- main.py
+++ main.py
@@ -1,26 +1,53 @@
 from bs4 import BeautifulSoup
-import requests
+import requests, re
+import time
 
 URL_main = 'https://www.dlsu.edu.ph/'
+links_visited = []
+links_scraped = []
 
 def getLinks(url):
-    # Fetch all the HTML source from the url
     response = requests.get(url)
-
-    soup = BeautifulSoup(response.text, 'html.parser')
-    links = soup.select('a')
+    bs = BeautifulSoup(response.text, 'html.parser')
+    links = bs.select('a')
     links_text = []
 
     for link in links:
         if link.get('href') != None:
-            if 'https://' in link.get('href'):
-                if link.get('href') not in links_text:
-                    links_text.append(link.get('href'))
+            if 'https://' in link.get('href') or 'http://' in link.get('href'):
+                newlink = link.get('href')
+            elif link.get('href').startswith('www'):
+                newlink = 'https://' + link.get('href')
             else:
-                if link.get('href') not in links_text:
-                    links_text.append(URL_main + link.get('href'))
+                newlink = URL_main + link.get('href')
+            
+            newlink = re.sub(r'(?<=\w)(\/\/\/|\/\/)(?=\w)', "/", newlink) #remove extra slashes
+
+            if newlink not in links_text and newlink.startswith('https://www.dlsu.edu.ph'):
+                links_text.append(newlink)
 
     return links_text
 
-for x in getLinks('https://www.dlsu.edu.ph/'):
-    print(x)
+def getEmails(url):
+    re_email = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+
+    response = requests.get(url)
+    bs = BeautifulSoup(response.text, 'html.parser')
+
+    emails = bs.select('span.__cf_email__')
+    emails_text = []
+
+    for email in emails:
+        if(re.search(re_email, cfDecodeEmail(email.get('data-cfemail')))):
+            emails_text.append(cfDecodeEmail(email.get('data-cfemail')))
+    
+    return emails_text
+
+def cfDecodeEmail(encodedString):
+    r = int(encodedString[:2],16)
+    email = ''.join([chr(int(encodedString[i:i+2], 16) ^ r) for i in range(2, len(encodedString), 2)])
+    return email
+
+# for x in getEmails('https://www.dlsu.edu.ph/offices/osa/constituent-offices/'):
+#     print(x)
+# getEmails('https://www.dlsu.edu.ph/offices/osa/constituent-offices/')

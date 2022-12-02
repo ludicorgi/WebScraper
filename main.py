d4247b834d299776b5f11929b6298074eca940cb
ludicorgi <69206279+ludicorgi@users.noreply.github.com>
2022-12-03 05:46:21 +0800

    basic consumer/producer implementation

--- main.py
+++ main.py
@@ -1,10 +1,51 @@
 from bs4 import BeautifulSoup
-import requests, re
-import time
+import requests, re, queue, threading, time
 
 URL_main = 'https://www.dlsu.edu.ph/'
+q_links = queue.Queue()
+timeout = 0
+
+consumer_threads=2
+
 links_visited = []
 links_scraped = []
+
+class producer (threading.Thread):
+    def __init__(self, url):
+        threading.Thread.__init__(self)
+        self.url=url
+
+    def run(self):
+        global q_links
+        global timeout
+
+        i = 1
+        while True:
+            q_links.put(i) 
+            i += 1
+            time.sleep(0.3)
+
+            if time.time() > timeout:
+                break
+
+class consumer (threading.Thread):
+    def __init__(self, thread_ID):
+        threading.Thread.__init__(self)
+        self.list=[]
+        self.item=0
+        self.ID=thread_ID
+
+    def run(self):
+        global q_links
+        global timeout
+
+        while True:
+            self.item = q_links.get()
+            print ("Consumer %i got an item %i \n"%(self.ID ,self.item))
+            time.sleep(1)
+
+            if time.time() > timeout:
+                break
 
 def getLinks(url):
     response = requests.get(url)
@@ -48,6 +89,24 @@ def cfDecodeEmail(encodedString):
     email = ''.join([chr(int(encodedString[i:i+2], 16) ^ r) for i in range(2, len(encodedString), 2)])
     return email
 
+if __name__=="__main__":
+    timeout = time.time() + 60*0.5
+    c_threads_list=[]
+
+    p = producer("url")
+
+    for n in range(consumer_threads):
+        c=consumer(n)
+        c_threads_list.append(c)
+        c.start()
+
+    p.start()
+
+    p.join()
+
+    for c in c_threads_list:
+        c.join()
+        
 # for x in getEmails('https://www.dlsu.edu.ph/offices/osa/constituent-offices/'):
 #     print(x)
 # getEmails('https://www.dlsu.edu.ph/offices/osa/constituent-offices/')
